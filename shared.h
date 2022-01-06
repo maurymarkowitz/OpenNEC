2662080fc45adf095491f4bff3cd97a4c9400291
Maury Markowitz <maury.markowitz@gmail.com>
2022-01-06 10:42:13 -0500

    added file header (comments)

--- shared.h
+++ shared.h
@@ -1,3 +1,13 @@
+/*******************************************************************
+ *
+ * shared.h
+ *
+ * shared.h contains forwards for shared externs used during
+ * calculations. needs to be replaced by a single structure that
+ * has all these parts in it so there's no globals
+ *
+ *******************************************************************/
+
 #ifndef SHARED_H
 #define SHARED_H	1
 
